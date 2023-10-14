14b9b883ac2a49bf06f57cf3623a2af630024226
Janina Reuter <stu204139@mail.uni-kiel.de>
2023-10-14 18:15:09 +0200

    New date 2024

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sat, 01 July 2023 8:00:00 GMT").getTime();
+var countDownDate = new Date("Sat, 29 June 2024 9:00:00 GMT").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
